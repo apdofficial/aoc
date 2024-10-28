2842c37dceec32cfd8ed6c9a95d175ca1e543a53
Andrej Pistek <andrej.pistek@outlook.com>
2024-10-28 19:48:09 +0100

    [day03] initial setup

---
+++ subprojects/aoc-2022/src/aoc/2022/day03.cc
@@ -0,0 +1,16 @@
+#include <aoc/2022/day03.hh>
+#include <aoc/common/puzzle_reader.hh>
+
+namespace aoc::aoc2022 {
+
+Day3::Day3(PuzzleReader::Lines const& lines) : lines_(lines) {}
+
+int Day3::SolvePart1() {
+  return -1;
+}
+
+int Day3::SolvePart2() {
+  return -1;
+}
+
+};  // namespace aoc::aoc2022
