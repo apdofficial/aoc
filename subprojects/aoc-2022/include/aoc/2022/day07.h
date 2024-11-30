0b372bfd44bbeecdef7ec9443afa18e0652f9f99
Andrej Pištek <andrej.pistek@outlook.com>
2024-11-30 19:58:37 +0100

    [day07] implemented day 7

--- subprojects/aoc-2022/include/aoc/2022/day07.h
+++ subprojects/aoc-2022/include/aoc/2022/day07.h
@@ -2,36 +2,11 @@
 
 #include <aoc/common/day.h>
 #include <aoc/common/puzzle/puzzle_reader.h>
-#include <string>
 
 namespace aoc::aoc2022 {
 
 class Day7 : public IDay {
  public:
-  enum class Command { cd, ls, invalid };
-
-  class Inode {
-   public:
-    Inode(std::string name, size_t size)
-        : name_{std::move(name)}, size_{size} {};
-    auto size() { return size_; }
-    auto name() { return name_; }
-
-   private:
-    std::string name_{};
-    size_t size_{};
-  };
-
-  class File : public Inode {
-   public:
-    File(std::string const& name, size_t size) : Inode(name, size){};
-  };
-
-  class Directory : public Inode {
-   public:
-    Directory(std::string const& name, size_t size) : Inode(name, size){};
-  };
-
   Day7(IPuzzleReader::Lines const&);
 
   [[nodiscard]] auto SolvePart1() -> Puzzle::Part1 override;
--- subprojects/aoc-2022/src/aoc/2022/day07.cc
+++ subprojects/aoc-2022/src/aoc/2022/day07.cc
@@ -1,15 +1,73 @@
 #include <aoc/2022/day07.h>
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <set>
+#include <span>
+#include <sstream>
+#include <string>
+
+namespace {
+
+auto CalcualteDirSizes(std::span<std::string> input_cmds)
+    -> std::map<std::string, int> {
+  std::map<std::string, int> dir_sizes{};
+  std::vector<std::string> current{};
+  for (auto const& cmd : input_cmds) {
+    if (cmd.starts_with("$ ls") || cmd.starts_with("dir")) {
+      continue;
+    }
+    if (cmd.starts_with("$ cd")) {
+      auto dest = cmd.substr(5, cmd.size());
+      if (dest == "..") {
+        current.pop_back();
+      } else {
+        current.push_back(dest);
+      }
+    } else {
+      auto size = std::stoi(cmd.substr(0, cmd.find(" ")));
+      auto tmp = current;
+      const char* const delim = "/";
+      while (!tmp.empty()) {
+        std::ostringstream key;
+        std::copy(tmp.begin(), tmp.end(),
+                  std::ostream_iterator<std::string>(key, delim));
+        auto val = dir_sizes[key.str()];
+        dir_sizes[key.str()] = val == 0 ? size : val + size;
+        tmp.pop_back();
+      }
+    }
+  }
+  return dir_sizes;
+}
+
+}  // namespace
 
 namespace aoc::aoc2022 {
 
 Day7::Day7(IPuzzleReader::Lines const& lines) : lines_{lines} {}
 
 auto Day7::SolvePart1() -> Puzzle::Part1 {
-  return -1;
+  auto dir_sizes = CalcualteDirSizes(lines_);
+  int sum{};
+  for (auto const& [k, v] : dir_sizes) {
+    if (v <= 100000) {
+      sum += v;
+    }
+  }
+  return sum;
 }
 
 auto Day7::SolvePart2() -> Puzzle::Part2 {
-  return -1;
+  auto dir_sizes = CalcualteDirSizes(lines_);
+  int minsize = dir_sizes["//"] - 40000000;
+  std::set<int> candidates{};
+  for (auto const& [k, v] : dir_sizes) {
+    if (v >= minsize) {
+      candidates.insert(v);
+    }
+  }
+  return *candidates.begin();
 }
 
 };  // namespace aoc::aoc2022
