75693549f6a0cb89ac4eeb399c9d37074ba8a4f6
Andrej Pištek <andrej.pistek@outlook.com>
2024-12-01 18:17:25 +0100

    use C++23

--- subprojects/aoc-2024/src/aoc/2024/day01.cc
+++ subprojects/aoc-2024/src/aoc/2024/day01.cc
@@ -1,44 +1,35 @@
 #include <aoc/2024/day01.h>
 
 #include <algorithm>
-#include <cmath>
 #include <map>
+#include <ranges>
 #include <sstream>
 
 namespace aoc::aoc2024 {
 
 Day1::Day1(IPuzzleReader::Lines const& lines) {
   for (auto const& line : lines) {
-    group_a_.push_back(0);
-    group_b_.push_back(0);
     std::istringstream is{line};
-    is >> group_a_.back() >> group_b_.back();
+    is >> group_a_.emplace_back() >> group_b_.emplace_back();
   }
 }
 
 auto Day1::SolvePart1() -> Puzzle::Part1 {
   std::ranges::sort(group_a_);
   std::ranges::sort(group_b_);
-  int total_distance{};
-  for (size_t i{}; i < group_a_.size(); ++i) {
-    total_distance += std::abs(group_a_[i] - group_b_[i]);
-  }
-  return total_distance;
+  return std::ranges::fold_left(
+      std::views::zip(group_a_, group_b_), 0, [](int acc, auto pair) {
+        return acc + std::abs(pair.first - pair.second);
+      });
 }
 
 auto Day1::SolvePart2() -> Puzzle::Part2 {
-  std::map<int, int> cache{};
-  int similartity{};
-  for (size_t i{}; i < group_a_.size(); ++i) {
-    auto const id = group_a_[i];
-    if (cache.contains(id)) {
-      similartity += cache[id];
-      continue;
-    }
-    cache[id] = id * std::ranges::count(group_b_, id);
-    similartity += cache[id];
+  std::map<int, int> count{};
+  for (auto const& id : group_b_) {
+    ++count[id];
   }
-  return similartity;
+  return std::ranges::fold_left(
+      group_a_, 0, [&count](int acc, int id) { return acc + id * count[id]; });
 }
 
 }  // namespace aoc::aoc2024
